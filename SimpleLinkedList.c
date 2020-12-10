0be56fd77ccfe789bc62e88d9ca609713e8a8620
stefanjiroveanu <58980054+stefanjiroveanu@users.noreply.github.com>
2020-12-10 10:05:27 +0200

    Update SimpleLinkedList.c

--- SimpleLinkedList.c
+++ SimpleLinkedList.c
@@ -8,11 +8,13 @@ typedef struct nod{
 
 typedef struct{
     nod *firstNode;
+    nod *lastNode;
 }list;
 
 list * alocList(){
     list *lista = (list*)malloc(sizeof(list));
     lista->firstNode = NULL;
+    lista->lastNode = NULL;
     return lista;
 };
 void addInt(list *l, int num){
@@ -20,6 +22,7 @@ void addInt(list *l, int num){
     Node = (nod*)malloc(sizeof(nod));
     Node->num = num;
     Node->nextNode = NULL;
+    l->lastNode = Node;
     nod *p = l->firstNode;
     if(p != NULL){
         while(p->nextNode != NULL)
@@ -29,6 +32,36 @@ void addInt(list *l, int num){
         p->nextNode = Node;
     }
     else l->firstNode = Node;
+}
+void addFirst(list *l, int num){
+    nod *Node;
+    Node = (nod*)malloc(sizeof(nod));
+    Node->nextNode = NULL;
+    Node->num = num;
+    if(l->firstNode == NULL){
+        l->lastNode = Node;
+        l->firstNode = Node;
+    }
+    else{
+            Node->nextNode = l->firstNode;
+            l->firstNode = Node;
+    }
+}
+void addLast(list *l, int num){
+    nod *Node;
+    Node = (nod*)malloc(sizeof(nod));
+    Node->nextNode = NULL;
+    Node->num = num;
+    if(l->firstNode == NULL)
+    {
+        l->firstNode = Node;
+        l->lastNode = Node;
+    }
+    else
+    {
+        l->lastNode->nextNode = Node;
+        l->lastNode = Node;
+    }
 }
 void afisareLista(list *l){
     nod *p;
@@ -42,10 +75,16 @@ void afisareLista(list *l){
 int main(){
     list *List;
     List = alocList();
-    addInt(List, 1);
+    /*addInt(List, 1);
     addInt(List, 2);
     addInt(List, 3);
-    addInt(List, 4);
+    addInt(List, 4);*/
+    addFirst(List,1);
+    addFirst(List,2);
+    addFirst(List,3);
+    addFirst(List,4);;
+    addInt(List,5);
+    addLast(List,6);
     afisareLista(List);
     return 0;
 }
